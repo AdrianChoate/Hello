447b19d0bfdb764593b1e8cee69e70b5f03d324c
AdrianChoate <54287964+AdrianChoate@users.noreply.github.com>
2019-08-29 18:41:55 -0500

    Added Major.
    Added my major.

---
+++ hello.c
@@ -0,0 +1,14 @@
+/**
+* Adrian Choate
+* 08/29/2019
+* Simple hello world type program
+*/
+ #include <stdlib>
+ #include <stdio>
+
+ int main(int argc, char **argv) {
+
+   printf("Adrian!\n");
+   printf("Computer Science!\n")
+   return 0;
+ }
